a6a67058a188a27976a41d6c4995c9b7f8697656
vijisrk22 <vivekaanand@gmail.com>
2026-04-30 06:49:22 -0400

    Fix CORS for production origin

--- BACKEND/config/cors.php
+++ BACKEND/config/cors.php
@@ -16,6 +16,8 @@ return [
         'http://localhost:3000',
         'http://localhost:8000',
         'http://localhost:5173',
+        'https://jolly-island-01b6ff91e.1.azurestaticapps.net',
+        'https://happy-pebble-0f4bc7310.6.azurestaticapps.net',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
