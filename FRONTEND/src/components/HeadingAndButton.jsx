5301a2466190742da223b1706c9146b6970b0cf5
vijisrk22 <vivekaanand@gmail.com>
2026-05-04 16:24:54 -0400

    Vivek changes 5/4 - Fixed button alignment in HeadingAndButton on mobile

--- FRONTEND/src/components/HeadingAndButton.jsx
+++ FRONTEND/src/components/HeadingAndButton.jsx
@@ -2,12 +2,12 @@ import ButtonRight from "./ButtonRight";
 
 function HeadingAndButton({ description, buttonText, path, disable = false, orangeArrow = false }) {
   return (
-    <div className="flex-grow">
-      <div className="py-2 flex flex-col justify-center items-start gap-3">
-        <div className="max-w-[300px] md:max-w-[400px] text-gray-800 text-lg sm:text-[18px] md:text-[20px] lg:text-[22px] font-medium font-dmsans leading-tight">
+    <div className="flex-grow w-full">
+      <div className="py-2 flex flex-col justify-center items-center lg:items-start gap-3">
+        <div className="max-w-[300px] md:max-w-[400px] text-gray-800 text-lg sm:text-[18px] md:text-[20px] lg:text-[22px] font-medium font-dmsans leading-tight text-center lg:text-start">
           {description}
         </div>
-        <div>
+        <div className="flex justify-center lg:justify-start w-full">
           <ButtonRight 
             text={buttonText} 
             path={path} 
